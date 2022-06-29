683dbdc5414d8a7411bc121b61d14543ac99dafc
SMANTOOR <SMANTOOR@SMANTOOR-7320.idc.oracle.com>
2022-06-29 15:12:31 +0530

    history clearing

--- genericinstall.sh
+++ genericinstall.sh
@@ -722,5 +722,5 @@ cleanup
 echo "Removing history for oracle user"
 runuser -l oracle -c "history -c && history -w && exit"
 echo "Removing history for root user"
-sudo history -c && history -w && exit
+history -c && history -w && exit
 sudo reboot
