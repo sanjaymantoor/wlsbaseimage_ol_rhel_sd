7ba15a46a6d87944f6ad2c43816fbf144db6418c
SMANTOOR <SMANTOOR@SMANTOOR-7320.idc.oracle.com>
2022-06-29 15:38:12 +0530

    Checking rebooted

--- genericinstall.sh
+++ genericinstall.sh
@@ -723,4 +723,6 @@ echo "Removing history for oracle user"
 runuser -l oracle -c "history -c && history -w && exit"
 echo "Removing history for root user"
 history -c && history -w && exit
+sudo last reboot
 sudo reboot
+echo "Rebooted"
