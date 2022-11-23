5814912cd2a06792217539c0857f3fa5bf24cb22
Sanjay Mantoor <36834780+sanjaymantoor@users.noreply.github.com>
2022-11-23 11:07:54 +0530

    Temporarily added polkit upgrade

--- genericinstall.sh
+++ genericinstall.sh
@@ -439,8 +439,9 @@ function updateOLOS()
 	minorVersion=`echo $osVersion |cut -f2 -d"."`
 	echo "Kernel version before update:"
 	uname -a
-	echo yum upgrade -y --disablerepo=ol7_latest  --enablerepo=ol${majorVersion}_u${minorVersion}_base
-	yum upgrade -y --disablerepo=ol7_latest  --enablerepo=ol${majorVersion}_u${minorVersion}_base
+	#echo yum upgrade -y --disablerepo=ol7_latest  --enablerepo=ol${majorVersion}_u${minorVersion}_base
+	#yum upgrade -y --disablerepo=ol7_latest  --enablerepo=ol${majorVersion}_u${minorVersion}_base
+	yum upgrade -y polkit
 	echo "Kernel version after update:"
 	uname -a
 }
