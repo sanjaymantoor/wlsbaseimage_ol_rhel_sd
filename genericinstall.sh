91643f9507bc2a8c74da773d8eea5b8dc9f6b5bf
sanjay.mantoor@oracle.com <sanjay.mantoor@oracle.com>
2023-05-12 05:57:33 +0000

    Added exit status code

--- genericinstall.sh
+++ genericinstall.sh
@@ -744,8 +744,8 @@ cleanup
 
 echo "Removing history for oracle user"
 runuser -l oracle -c "history -c && history -w && exit"
-echo "Removing history for root user"
-sudo history -c && history -w
+#echo "Removing history for root user"
+#sudo history -c && history -w
 ec=$?
 echo "Status code : $ec"
 exit $ec
